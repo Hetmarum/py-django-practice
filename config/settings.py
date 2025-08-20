d5555149b2823cb0758c78e199d796ed39cace56
Hetmarum <kpxmedet@gmail.com>
2025-08-20 11:57:14 +0300

    Added crispy_forms deleted namespace & fixed sidebar

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "crispy_bootstrap4"
+    "crispy_bootstrap4",
+    "crispy_forms",
     "tasks"
 ]
 
