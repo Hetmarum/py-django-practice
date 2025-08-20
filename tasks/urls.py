7ca6aa9f9c3244478464041e55147c8135fa6a60
Hetmarum <kpxmedet@gmail.com>
2025-08-20 11:59:25 +0300

    Added new paths new views, fixed button in index.html

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
 
-from tasks.views import IndexView
+from tasks.views import (
+    IndexView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView
+)
 
 app_name = "tasks"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
+
+from tasks.models import Tag
 
 
 class IndexView(TemplateView):
     template_name = "tasks/index.html"
+
+
+class TagListView(ListView):
+    model = Tag
+    template_name = "tasks/tags_list.html"
+    context_object_name = "tag_list"
+
+
+class TagCreateView(CreateView):
+    model = Tag
+    fields = "__all__"
+    template_name = "tasks/tag_form.html"
+    success_url = reverse_lazy("tasks:tag-list")
+
+
+class TagUpdateView(UpdateView):
+    pass
+
+class TagDeleteView(DeleteView):
+    pass
