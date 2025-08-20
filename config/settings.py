ddb6c1568757e3667dd4fc67d7b6de9def93dc47
Hetmarum <kpxmedet@gmail.com>
2025-08-20 11:09:21 +0300

    Added crispy-bootstrap4 in settings & base.html

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_bootstrap4"
     "tasks"
 ]
 
@@ -123,3 +124,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "tasks.User"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
